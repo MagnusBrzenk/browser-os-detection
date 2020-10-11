f8b93a21b5e6e1a7cff434a996ef80a0ba54ce54
MagnusBrzenk <magnusbrzenk@gmail.com>
2020-10-11 17:28:22 -0400

    Updates

---
+++ precache-manifest.72ae5c49af1f4f5dc9617d8475a2e1cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc2061d5cc9e35949f3561bdcd905432",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33b372f5b422e9931968",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c76eb1a67f4135a3541a",
+    "url": "/static/js/2.c5e39bbf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c5e39bbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33b372f5b422e9931968",
+    "url": "/static/js/main.194ee8f4.chunk.js"
+  },
+  {
+    "revision": "aaff1e6ce9a0b8e9f006",
+    "url": "/static/js/runtime-main.9cd18b8a.js"
+  }
+]);
